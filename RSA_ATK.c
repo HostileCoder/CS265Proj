d08bd6e0db910b3d7135716038583b27e251235c
sai <chin.tsai@sjsu.edu>
2016-02-21 17:44:18 -0800

    d

--- RSA_ATK.c
+++ RSA_ATK.c
@@ -60,15 +60,21 @@ void BigFFA(mpz_t N){
 
 
 void BigFFA2(mpz_t N){
-	mpz_t a,b2,one,rem,sq,ans,bsq;
+	mpz_t a,b2,one,rem,sq,ans,b,Pm1,Qm1,p,q,pqM1,d,e,c;
 	mpz_init_set_str(one, "1", 10);
 	mpz_init_set_str(b2, "1", 10);
 	mpz_init_set_str(a, "1", 10);
 	mpz_init_set_str(rem, "1", 10);
 	mpz_init_set_str(sq, "1", 10);
 	mpz_init_set_str(ans, "1", 10);
-	mpz_init_set_str(bsq, "1", 10);
-
+	mpz_init_set_str(b, "1", 10);
+	mpz_init_set_str(Pm1, "1", 10);
+	mpz_init_set_str(Qm1, "1", 10);
+	mpz_init_set_str(p, "1", 10);
+	mpz_init_set_str(q, "1", 10);
+	mpz_init_set_str(pqM1, "1", 10);
+	mpz_init_set_str(d, "1", 10);
+	mpz_init_set_str(e, "17", 10);
 	mpz_sqrtrem(a,rem,N);
 	mpz_add(a,a,one);
 	mpz_mul(sq, a, a);
@@ -80,13 +86,33 @@ void BigFFA2(mpz_t N){
 		mpz_sub(b2, sq, N);
 	}
 
-	mpz_sqrt(bsq,b2);
-	mpz_sub(ans, a, bsq);
+	mpz_sqrt(b,b2);
+	mpz_sub(p, a, b);
+	mpz_add(q, a, b);
 
-	gmp_printf("%Zd\n",ans);
+	mpz_sub(Pm1, p, one);
+	mpz_sub(Qm1, q, one);
+	mpz_mul(pqM1, Pm1, Qm1);
+	mpz_invert(d,e,pqM1);
+
+	gmp_printf("p:   		%Zd\n",p);
+	gmp_printf("q:   		%Zd\n",q);
+	gmp_printf("p-1: 		%Zd\n",Pm1);
+	gmp_printf("q-1: 		%Zd\n",Qm1);
+	gmp_printf("(p-1)(q-1): %Zd\n",pqM1);
+	gmp_printf("d: 			%Zd\n",d);
+
+	mpz_init_set_str(c, "6553737221362549160467829576342303867230194078458614544455788969208029481986028792492547434668066713199741381758065961061995002107860154452799857795971736508536768368771616799261668162539134913957419310641713821039539526627103122788149293368223969280354360846637109616466207418440363036312473649455733021730", 10);
+	Decrypt(d,N,c);
 
 }
 
+void Decrypt(mpz_t d,mpz_t N,mpz_t c){
+	mpz_t m;
+	mpz_init_set_str(m, "0", 10);
+	mpz_powm_sec(m,c,d,N);
+	gmp_printf("m:%Zd\n",m);
+}
 
 int FFA(int n){
 	if(n%2==0)
