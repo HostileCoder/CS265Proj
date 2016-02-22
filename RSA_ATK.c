a2e377fba19ea8bd54c1174a8a66fb8f9569c3ca
sai <chin.tsai@sjsu.edu>
2016-02-21 16:23:58 -0800

    f

--- RSA_ATK.c
+++ RSA_ATK.c
@@ -10,13 +10,14 @@
 #include <gmp.h>
 
 void BigFFA(mpz_t);
+void BigFFA2(mpz_t);
 
 int main(int argc, char** argv) {
 	//printf("%d\n",FFA(221));
 
 	mpz_t N;
-	mpz_init_set_str(N, "221", 10);
-	BigFFA(N);
+	mpz_init_set_str(N, "316033277426326097045474758505704980910037958719395560565571239100878192955228495343184968305477308460190076404967552110644822298179716669689426595435572597197633507818204621591917460417859294285475630901332588545477552125047019022149746524843545923758425353103063134585375275638257720039414711534847429265419", 10);
+	BigFFA2(N);
 	mpz_clear(N);
 }
 
@@ -41,9 +42,8 @@ void BigFFA(mpz_t N){
 	}
 
 
-
-
 	while(1){
+		 printf("%d\n","d");
 		 mpz_mul(Ysqtr, y, y);
 		 mpz_add(NplusYsq,Ysqtr,N);
 		 if(mpz_perfect_square_p(NplusYsq)!=0){
@@ -54,11 +54,39 @@ void BigFFA(mpz_t N){
 
 	mpz_sqrt(NplusYsq,NplusYsq);
 	mpz_sub(ans, NplusYsq, y);
-//	gmp_printf(":%Zd\n",NplusYsq);
-//	gmp_printf(":%Zd\n",y);
 	gmp_printf("%Zd\n",ans);
 
 }
+
+
+void BigFFA2(mpz_t N){
+	mpz_t a,b2,one,rem,sq,ans,bsq;
+	mpz_init_set_str(one, "1", 10);
+	mpz_init_set_str(b2, "1", 10);
+	mpz_init_set_str(a, "1", 10);
+	mpz_init_set_str(rem, "1", 10);
+	mpz_init_set_str(sq, "1", 10);
+	mpz_init_set_str(ans, "1", 10);
+	mpz_init_set_str(bsq, "1", 10);
+
+	mpz_sqrtrem(a,rem,N);
+	mpz_add(a,a,one);
+	mpz_mul(sq, a, a);
+	mpz_sub(b2, sq, N);
+
+	while(mpz_perfect_square_p(b2)==0){
+		mpz_add(a,a,one);
+		mpz_mul(sq, a, a);
+		mpz_sub(b2, sq, N);
+	}
+
+	mpz_sqrt(bsq,b2);
+	mpz_sub(ans, a, bsq);
+
+	gmp_printf("%Zd\n",ans);
+
+}
+
 
 int FFA(int n){
 	if(n%2==0)
